056f2563c8b82faac37123b9829bca6d84ce3685
SNIAPA <wasilewskihuberts@gmail.com>
2023-11-05 15:32:12 +0100

    lib cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 use js_sys::JsString;
 use log::*;
-use screeps::{game, console};
+use screeps::{console, game};
 use wasm_bindgen::prelude::*;
 
-use crate::{
-     manager::Manager, mem::creep::clean_creeps, room::RoomManager,
-    spawn::SpawnManager,
-};
+use crate::{manager::Manager, mem::creep::clean_creeps, room::RoomManager, spawn::SpawnManager};
 
 mod creep;
 mod logging;
@@ -20,6 +17,7 @@ mod util;
 pub fn setup() {
     logging::setup_logging(logging::Trace);
     info!("setup");
+
     RoomManager::setup().unwrap();
     creep::setup().unwrap();
 }
@@ -27,13 +25,19 @@ pub fn setup() {
 #[wasm_bindgen(js_name = loop)]
 pub fn game_loop() {
     clean_creeps().unwrap();
+
     RoomManager::run_all().unwrap();
+
     creep::run_all().unwrap();
+
+    print_loop_stats()
+}
+fn print_loop_stats() {
     let heap_stats = game::cpu::get_heap_statistics();
     web_sys::console::log_1(&JsString::from(format!(
         "<font size=\"+3\">{} cpu:{}% mem:{}%</font>",
         game::time(),
-        (game::cpu::get_used()/game::cpu::tick_limit() * 100.0).round(),
-        (heap_stats.used_heap_size()/heap_stats.heap_size_limit() * 100) ,
+        (game::cpu::get_used() / game::cpu::tick_limit() * 100.0).round(),
+        (heap_stats.used_heap_size() / heap_stats.heap_size_limit() * 100),
     )));
 }
